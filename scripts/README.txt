5dc1665d854c5eeae8ceae11c99a3156fce1fba8
Binita Bharati <binita.bharati@gmail.com>
2013-11-02 12:56:04 +0530

    Adding scripts folder

---
+++ scripts/README.txt
@@ -0,0 +1,7 @@
+ Welcome to JMSChirp ! Your friendly JMS browser !
+ 
+ Prerequiste : Java6 or higher should be present.
+
+ To run the process , execute the below command :
+ java -classpath .:<provider specific jar files>:jmschirp-0.1.0-SNAPSHOT-standalone.jar jmschirp.main
+ 
---
+++ scripts/packager.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd ..
+lein clean
+lein uberjar
+mkdir -p target/jmschirp-0.1.0
+cp target/jmschirp-0.1.0-SNAPSHOT-standalone.jar target/jmschirp-0.1.0
+cp scripts/README.txt target/jmschirp-0.1.0
+cp scripts/log4j.properties target/jmschirp-0.1.0
+cd target
+tar -czvf jmschirp-0.1.0.tar.gz jmschirp-0.1.0/ 
+zip -r jmschirp-0.1.0.zip jmschirp-0.1.0/
