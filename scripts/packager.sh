7c3e14f593eb60af6e475e60dc1dbbb173d2b4e4
Binita Bharati <binita.bharati@gmail.com>
2013-11-03 21:12:50 +0530

    Made sure that package script is only run from the project root

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-cd ..
+
+VERSION=0.1.0
+PWD=`pwd | awk -F/ '{ print $NF; }'`
+
+if [ $PWD !=  'jmschirp' ]; then 
+  echo "You must run the script from the project root directory"
+  exit 1
+fi
+
 lein clean
 lein uberjar
 mkdir -p target/jmschirp-0.1.0
@@ -9,3 +17,4 @@ cp scripts/log4j.properties target/jmschirp-0.1.0
 cd target
 tar -czvf jmschirp-0.1.0.tar.gz jmschirp-0.1.0/ 
 zip -r jmschirp-0.1.0.zip jmschirp-0.1.0/
+
